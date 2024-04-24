13d91dc8b73b1354076ab770796dbf63148bae64
Reo Sahobby <reoshby@gmail.com>
2024-04-24 10:35:33 +0700

    update v1.2

--- dto/requestLogin.go
+++ dto/requestLogin.go
@@ -4,3 +4,7 @@ type RequestLogin struct {
 	Email    string `json:"email,omitempty"`
 	Password string `json:"password,omitempty"`
 }
+
+type LoginResponse struct {
+	Token string `json:"token,omitempty"`
+}
